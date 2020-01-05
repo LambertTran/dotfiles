aee3960e8470dc8b9a7195f1729b433d68c06db6
Lambert Tran <lb.tran648@gmail.com>
2020-01-05 10:00:58 -0800

    fix setup dotfile

--- setup.sh
+++ setup.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
+echo "----- Link bash_aliases ------"
 rm ~/.bash_aliases 
-rm ~/.emacs
 ln -s ${PWD}/.bash_aliases ~/.bash_aliases
+
+echo "----- Make emacs backup folder -----"
+rm -rf .saves
+
+echo "----- Link emacs ------"
+rm ~/.emacs
 ln -s ${PWD}/.emacs ~/.emacs
